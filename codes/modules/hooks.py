11e43e806ad18a90550b1a92cc38e5dac7a3ba74
cdgyp <cdgyp233@qq.com>
2023-05-13 20:19:05 +0800

    修复范数类型

--- codes/modules/hooks.py
+++ codes/modules/hooks.py
@@ -4,21 +4,25 @@ from codes.base.base import BaseModule, Plugin
 from einops import einsum
 
 from ..base import ForwardHook, BackwardHook, Hook, Plugin, replace_config
-from vit_pytorch.vit import ViT
+from .vit import ViT, FeedForward
 
 
 class ActivationHook(Hook):
     def hook_on_all(vit: ViT, *args, **kwargs):
         hook_type = kwargs['type']  if 'type'  in kwargs and kwargs['type'] is not None else ActivationHook
+        module_types = kwargs['module_types']
 
         res = []
         for name, module in vit.named_modules():
             if not '1.fn' in name:
                 continue
-            if isinstance(module, nn.GELU) or isinstance(module, nn.ReLU):
-                h = hook_type()
-                h.hook_on(module, name)
-                res.append(h)
+            for type in module_types:
+                if isinstance(module, type):
+                    h = hook_type()
+                    h.hook_on(module, name)
+                    res.append(h)
+                    break
+        
         return res
 
 
@@ -31,7 +35,7 @@ class ActivationMapHook(ForwardHook):
         self.activations = output
 
     def hook_on_all(module: nn.Module, *args, **kwargs):
-        return ActivationHook.hook_on_all(module, *args, **replace_config(kwargs, type=ActivationMapHook))
+        return ActivationHook.hook_on_all(module, *args, **replace_config(kwargs, type=ActivationMapHook, module_types=[nn.ReLU, nn.GELU]))
 
 class ActivationGradientHook(BackwardHook):
     def __init__(self) -> None:
@@ -43,14 +47,17 @@ class ActivationGradientHook(BackwardHook):
         assert isinstance(grad_input, torch.Tensor), grad_input
         self.gradients = grad_input
     def hook_on_all(module: nn.Module, *args, **kwargs):
-        return ActivationHook.hook_on_all(module, *args, **replace_config(kwargs, type=ActivationGradientHook))
+        return ActivationHook.hook_on_all(module, *args, **replace_config(kwargs, type=ActivationGradientHook, module_types=[FeedForward]))
     
 
 class ActivationObservationPlugin(Plugin):
-    def __init__(self):
+    def __init__(self, beta=0.9):
         super().__init__()
         self.activation_hooks: list[ActivationMapHook] = []
         self.gradient_hooks: list[ActivationGradientHook] = []
+        self.gradient_ratios = 1
+        self.diagonal_gradients = 0
+        self.beta = beta
     def register(self, main: BaseModule, plugins: 'list[Plugin]'):
         self.activation_hooks = ActivationMapHook.hook_on_all(main)
         self.gradient_hooks = ActivationGradientHook.hook_on_all(main)
@@ -82,6 +89,8 @@ class ActivationObservationPlugin(Plugin):
 
         with torch.no_grad():
             assert self.training
+            gradient_ratios = []
+            diagonal_gradients = []
             for i,  h in enumerate(self.gradient_hooks):
                 assert h.handle is not None
                 g = h.gradients
@@ -93,11 +102,22 @@ class ActivationObservationPlugin(Plugin):
 
                 identity = torch.eye(ggT.shape[-1], device=ggT.device).unsqueeze(dim=0)
 
-                diagonal = (ggT * identity).norm(dim=[-1, -2], p=2) ** 2
-                non_diagonal = (ggT * (1 - identity)).norm(dim=[-1, -2], p=2) ** 2
+                # diagonal: torch.Tensor = (ggT * identity).abs().sum(dim=[-1, -2])
+                # non_diagonal: torch.Tensor = (ggT * (1 - identity)).abs().sum(dim=[-1, -2])
+                diagonal: torch.Tensor = (ggT * identity).norm(p=1, dim=[-1, -2])
+                non_diagonal: torch.Tensor = (ggT * (1 - identity)).norm(p=1, dim=[-1, -2])
                 assert len(diagonal.shape) == 1 and len(non_diagonal.shape) == 1
                 self.losses.observe(diagonal, 'gradient', 'diagonal', str(i))
                 self.losses.observe(non_diagonal, 'gradient', 'non-diagonal', str(i))
+                self.losses.observe((diagonal / (non_diagonal + 1e-32)).log10(), 'gradient_ratio', str(i))
+
+                gradient_ratios.append((diagonal / (non_diagonal + 1e-32)).log10().mean())
+                diagonal_gradients.append(diagonal.mean())
+
+            gradient_ratios = torch.stack(gradient_ratios).flatten()
+            diagonal_gradients = torch.stack(diagonal_gradients).flatten()
+            self.gradient_ratios = self.beta * self.gradient_ratios + (1 - self.beta) * gradient_ratios
+            self.diagonal_gradients = self.beta * self.diagonal_gradients + (1 - self.beta) * diagonal_gradients
 
 
     def clean(self):
