b4480da61da716bc21c50b2ef51ced25e1445628
彭 泽 <pengze@smail.nju.edu.cn>
2023-06-22 00:20:22 +0800

    去除多余的参数

--- codes/modules/robustness.py
+++ codes/modules/robustness.py
@@ -5,10 +5,9 @@ from ..base import BaseModule
 class ImplicitAdversarialSample(BaseModule):
     def __init__(self) -> None:
         super().__init__()
-        self.device_tester = nn.Parameter(torch.tensor([0.0]))
         self.biases: torch.Tensor = None
     def forward(self, x: torch.Tensor):
         if self.biases is None:
-            self.biases = nn.Parameter(torch.zeros(x.shape[1:], device=self.device_tester.device))
+            self.biases = nn.Parameter(torch.zeros(x.shape[1:], device=x.device))
         self.losses.observe(self.biases.abs().mean(), 'implicit_adversarial_samples')
         return x + self.biases
