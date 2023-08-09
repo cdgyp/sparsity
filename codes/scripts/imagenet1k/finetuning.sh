af2ab2181dea85fa1e6c3de267ddaa776d826ebc
彭 泽 <pengze@smail.nju.edu.cn>
2023-08-09 16:26:27 +0800

    分离 imagenet 上的从头训练和微调

--- codes/scripts/imagenet1k/finetuning.sh
+++ codes/scripts/imagenet1k/finetuning.sh
@@ -5,14 +5,12 @@ export https_proxy=
 for model in ${MODELS[@]};
 do
     python -m codes.scripts.imagenet1k.imagenet1k   \
-        --model $model --start-epoch 0 --epochs 300 --batch-size 512 --physical-batch-size 64 --opt adamw --lr 0.003 --wd 0.3 \
-        --from-scratch\
+        --model $model --start-epoch 0 --epochs 10 --batch-size 512 --physical-batch-size 32 --opt adamw --lr 0.003 --wd 0.3 \
         --data-path 'data/imagenet1k256/ILSVRC/Data/CLS-LOC'    \
-        --lr-scheduler cosineannealinglr    \
+        --lr-scheduler sineannealinglr  \
         --label-smoothing 0.11 --mixup-alpha 0.2 --auto-augment ra    \
-        --clip-grad-norm 1 --ra-sampler --cutmix-alpha 1.0 \
+        --clip-grad-norm 10 --ra-sampler --cutmix-alpha 1.0 \
         --amp \
-        --device $DEVICE  --log_per_step 20 --physical-epochs 300 \
-        --implicit-adversarial-samples-clipping 1.0\
+        --device $DEVICE  --log_per_step 20 --physical-epochs 10 \
         "$@"
 done
