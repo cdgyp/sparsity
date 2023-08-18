cd4e9d95d0e687eb8aa340788e5dfb1277d6638c
彭 泽 <pengze@smail.nju.edu.cn>
2023-08-18 15:35:35 +0800

    weight decay 使末期的等效学习率不断增大，因此在末期指数降低学习率

--- codes/scheduler/sine.py
+++ codes/scheduler/sine.py
@@ -1,13 +1,39 @@
 import torch
-from torch.optim.lr_scheduler import LambdaLR
+from torch.optim.lr_scheduler import LRScheduler
 import math
+import warnings
 
-class SineAnnealingScheduler(LambdaLR):
+class SineAnnealingScheduler(LRScheduler):
     def full_cosine(self, epoch):
-        lr = max(1 / 2 - math.cos(2*math.pi * min(epoch / self.T_max, 1)) / 2, self.min_eta_ratio)
-        print(lr)
-        return lr
-    def __init__(self, optimizer, T_max, last_epoch, verbose=False, min_eta_ratio=3e-2):
+        phase = 2 * math.pi * min(epoch, self.T_max - 1) / self.T_max
+        lr_ratio_base = 1 / 2 - math.cos(phase) / 2
+
+        lr_ratio = lr_ratio_base * (self.gamma ** max(0, epoch - (self.T_max - 1)))
+        return lr_ratio
+    
+    def __init__(self, optimizer, T_max, last_epoch, verbose=False, min_eta_ratio=3e-1, gamma=0.5):
         self.T_max = T_max
         self.min_eta_ratio=min_eta_ratio
-        super().__init__(optimizer, lr_lambda=self.full_cosine , last_epoch=last_epoch, verbose=verbose)
+        self.gamma = gamma
+        super().__init__(optimizer, last_epoch=last_epoch, verbose=verbose)
+    
+    def get_lr(self) -> float:
+        if not self._get_lr_called_within_step:
+            warnings.warn("To get the last learning rate computed by the scheduler, "
+                          "please use `get_last_lr()`.")
+
+        return [base_lr * self.full_cosine(self.last_epoch)
+                for base_lr in self.base_lrs]
+        
+
+if __name__ == '__main__':
+    model = torch.nn.Linear(10, 10)
+    optimizer = torch.optim.SGD(model.parameters(), lr=3e-3)
+    scheduler = SineAnnealingScheduler(optimizer, 10, -1, True)
+    for i in range(0, 15):
+        print(f"epoch {i}", scheduler.get_last_lr())
+        loss = model.weight.sum()
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        scheduler.step()
