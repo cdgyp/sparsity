dcf835997a7fab86b7ce2ecc60fb14b414c3898f
pengze <pengze@smail.nju.edu.cn>
2023-09-19 22:15:33 +0800

    每个 checkpoint 一次测试

--- codes/scripts/T5/T5.sh
+++ codes/scripts/T5/T5.sh
@@ -32,7 +32,7 @@ do
         --warmup_steps                  10000   \
         --logging_steps                 25      \
         --save_steps                    5000    \
-        --eval_steps                    2500     \
+        --eval_steps                    5000    \
         --from_disk                             \
         --dataset_name              'data/c4'   \
         --max_seq_length                512     \
