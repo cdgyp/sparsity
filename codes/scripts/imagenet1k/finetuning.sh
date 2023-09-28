136329b43731cabc252b9c0040870c9992202e42
pengze <pengze@smail.nju.edu.cn>
2023-09-29 02:47:37 +0800

    先使用较短的 epoch 测试稀疏化方案

--- codes/scripts/imagenet1k/finetuning.sh
+++ codes/scripts/imagenet1k/finetuning.sh
@@ -11,14 +11,14 @@ IFS=','
 for model in $MODELS;
 do
     torchrun --nproc_per_node=$n_visible_devices module_wrapper.py codes.scripts.imagenet1k.imagenet1k   \
-        --model $model --start-epoch 0 --epochs 15 --batch-size $batch_size_per_proc --physical-batch-size 64 --opt adamw --lr 0.003 --wd 0.3 \
+        --model $model --start-epoch 0 --epochs 10 --batch-size $batch_size_per_proc --physical-batch-size 64 --opt adamw --lr 0.003 --wd 0.3 \
         --data-path 'data/imagenet1k256/ILSVRC/Data/CLS-LOC'    \
         --lr-scheduler cosineannealinglr  \
         --label-smoothing 0.11 --mixup-alpha 0.2 --auto-augment ra    \
         --clip-grad-norm 1 --ra-sampler --cutmix-alpha 1.0 \
 	--workers 4 \
         --amp \
-        --log_per_step 100 --physical-epochs 15 \
+        --log_per_step 100 --physical-epochs 10 \
         --dont-resume-lr-schedulers \
         --finetune runs/finetuning/start/start.pth \
         --lora --lora-r 16  \
